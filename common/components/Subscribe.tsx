3500859d9e5f1e140c5d0a166908a2491e9763fe
Idlesome <kirtrainford@gmail.com>
2024-09-26 11:37:49 +0100

    fix: revert condition for subscribe script

--- common/components/Subscribe.tsx
+++ common/components/Subscribe.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 export const Subscribe = () => (
   <div className="width-65ch items-center">
-    {process.env.NODE_ENV === "production" && (
-      <script
-        async
-        data-uid="b72c4a9eaa"
-        src="https://channel-positivity.ck.page/b72c4a9eaa/index.js"
-      ></script>
-    )}
+    <script
+      async
+      data-uid="b72c4a9eaa"
+      src="https://channel-positivity.ck.page/b72c4a9eaa/index.js"
+    ></script>
+    )
   </div>
 );
